82a7c8913b6ae44eb01171cf1e5a996f59b3a966
Alavia <alavia.yahya@gmail.com>
2018-05-09 02:09:07 -0400

    hard-coded cli methods with temp info

--- lib/cap_bridge/cli.rb
+++ lib/cap_bridge/cli.rb
@@ -1,7 +1,20 @@
 class CapBridge::CLI
 
     def call
-        puts "Attractions at Capilano Suspension Bridge Park"
+        list_attractions
+        options
     end
 
+    def list_attractions
+        puts "Attractions at Capilano Suspension Bridge Park"
+        puts <<-DOC.gsub /^\s*/,''
+            1. Suspension Bridge
+            2. Cliffwalk
+            3. Treetops Adventure
+        DOC
+    end
+
+    def options
+        puts "Enter the number of the attraction you'd like to learn more about:"
+    end
 end
