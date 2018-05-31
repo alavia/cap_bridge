f111687185d242946036a78c3a4cd594c2ec1a5f
Alavia <alavia.yahya@gmail.com>
2018-05-30 22:24:41 -0400

    scraping more objects

--- lib/cap_bridge/attractions.rb
+++ lib/cap_bridge/attractions.rb
@@ -15,6 +15,8 @@ class CapBridge::Attractions
         attractions << self.scrape_storycentre
         attractions << self.scrape_kiapalano
         attractions << self.scrape_raptors
+        attractions << self.scrape_tours
+        attractions << self.scrape_forest
 
         attractions
     end
@@ -23,7 +25,7 @@ class CapBridge::Attractions
         doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
 
         attraction = self.new
-        attraction.name = doc.css("#menu-item-424").text #scrapes susp bridge title
+        attraction.name = doc.css("#menu-item-424").text.upcase #scrapes susp bridge title
         suspbridge_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/capilano-suspension-bridge/"))
         attraction.description = suspbridge_doc.css("p").children[1].text
         attraction
@@ -33,7 +35,7 @@ class CapBridge::Attractions
         doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
 
         attraction = self.new
-        attraction.name = doc.css("#menu-item-425").text
+        attraction.name = doc.css("#menu-item-425").text.upcase
         cliffwalk_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/cliffwalk/"))
         attraction.description = cliffwalk_doc.css("p").children[1].text
         attraction
@@ -43,7 +45,7 @@ class CapBridge::Attractions
         doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
 
         attraction = self.new
-        attraction.name = doc.css("#menu-item-430").text
+        attraction.name = doc.css("#menu-item-430").text.upcase
         treetops_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/treetops-adventure/"))
         attraction.description = treetops_doc.css("p").children[1..3].text
         attraction
@@ -53,7 +55,7 @@ class CapBridge::Attractions
         doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
 
         attraction = self.new
-        attraction.name = doc.css("#menu-item-429").text
+        attraction.name = doc.css("#menu-item-429").text.upcase
         storycentre_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/story-centre/"))
         attraction.description = storycentre_doc.css("p").children[1].text
         attraction
@@ -63,7 +65,7 @@ class CapBridge::Attractions
         doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
 
         attraction = self.new
-        attraction.name = doc.css("#menu-item-427").text
+        attraction.name = doc.css("#menu-item-427").text.upcase
         kiapalano_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/kiapalano/"))
         attraction.description = kiapalano_doc.css("p").children[1].text
         attraction
@@ -77,6 +79,27 @@ class CapBridge::Attractions
         raptors_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
         attraction.description = raptors_doc.css(".inner-text-hover p").children[16].text.strip
         attraction
+    end
+
+    def self.scrape_tours
+        doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
+
+        attraction = self.new
+        attraction.name = doc.css("#menu-item-426").text.upcase
+        tours_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/guided-tours/"))
+        attraction.description = tours_doc.css("p").children[1..2].text
+        attraction
+    end
+
+    def self.scrape_forest
+        doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/"))
+
+        attraction = self.new
+        attraction.name = doc.css("#menu-item-428").text.upcase
+        forest_doc = Nokogiri::HTML(open("https://www.capbridge.com/explore/living-forest/"))
+        attraction.description = forest_doc.css("p").children[0].text
+        attraction
+        binding.pry
     end
 
 end
